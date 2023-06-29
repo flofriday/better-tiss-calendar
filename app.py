f921cc01086d6da02a293d75c47a30360dc15821
flofriday <flohacksfriday@gmail.com>
2023-06-29 14:28:17 +0200

    Implement working frontend
    This is nothing fancy, but it works good enough for now to implement the
    backend.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, abort, request
 
 app = Flask(__name__)
 
@@ -11,3 +11,16 @@ def home():
 @app.route("/static/<path:path>")
 def send_report(path):
     return send_from_directory("static", path)
+
+
+@app.route("/verify")
+def verify():
+    url = request.args.get("url")
+    if url is None:
+        return "No url provided", 400
+    return "Ok"
+
+
+@app.route("/tiss.ical")
+def icalendar():
+    abort(501)
---
+++ static/home.js
@@ -0,0 +1,69 @@
+// Should we use typescript or a svelte for just this single page?
+
+const verifyBtn = document.getElementById("verify");
+const copyBtn = document.getElementById("copy");
+const urlText = document.getElementById("original_url");
+const betterText = document.getElementById("better_url");
+const betterTextPlaceholder = document.getElementById("better_url_placeholder");
+const errorText = document.getElementById("error_message");
+
+urlText.onchange = () => {
+  disableBetterUrl();
+};
+
+urlText.onkeydown = () => {
+  disableBetterUrl();
+};
+
+verifyBtn.onclick = async () => {
+  await verify(urlText.value);
+};
+
+async function verify(url) {
+  try {
+    const response = await fetch(`/verify?url=${encodeURIComponent(url)}`);
+    if (response.ok) {
+      setBetterUrl(url);
+    } else {
+      let message = await response.text();
+      if (message.trim().length > 0) {
+        setErrorMessage(message);
+      } else {
+        setErrorMessage("Server returned: " + response.statusText);
+      }
+    }
+  } catch (error) {
+    setErrorMessage("Could not connect to the server: " + error);
+  }
+}
+
+function disableBetterUrl() {
+  errorText.classList.add("hidden");
+  betterText.classList.add("hidden");
+  betterTextPlaceholder.classList.remove("hidden");
+  copyBtn.disabled = true;
+}
+
+function setErrorMessage(message) {
+  errorText.classList.remove("hidden");
+  errorText.innerText = "🔥 " + message;
+}
+
+function setBetterUrl(originalUrl) {
+  errorText.classList.add("hidden");
+  betterText.classList.remove("hidden");
+  betterTextPlaceholder.classList.add("hidden");
+  copyBtn.disabled = false;
+
+  const tmpurl = new URL(originalUrl);
+  const searchParams = new URLSearchParams(tmpurl.search);
+  const token = searchParams.get("token");
+  const locale = searchParams.get("locale");
+
+  const domain = window.location.origin;
+  const better_url = `${domain}/tiss.ical?token=${encodeURIComponent(
+    token
+  )}&locale=${encodeURIComponent(locale)}`;
+
+  betterText.innerText = better_url;
+}
