fa4553a86c8054c7e4504baef7f370f980788cf3
flofriday <flohacksfriday@gmail.com>
2023-07-02 19:18:02 +0200

    Add shorthands as the event names
    This isn't perfect, we still don't have a lot of lectures, and now
    it is necessary that you know the shorthand (which might be wrong).
    The solution for that is to include the original title in the
    description.
    But I am really happy that we got this far. ^^

--- better.py
+++ better.py
@@ -10,15 +10,23 @@ def improve_calendar(cal: Calendar) -> Calendar:
 
 
 def rename_subjects(cal: Calendar) -> Calendar:
+    shorthands = read_shorthands()
+
     for component in cal.walk():
         if component.name == "VEVENT":
             old_summary = component.get("summary")
             component.pop("summary")
-            component.add("summary", better_summary(old_summary))
+            component.add("summary", better_summary(old_summary, shorthands))
     return cal
 
 
-def better_summary(old: str) -> str:
+def read_shorthands() -> dict[str, str]:
+    with open("resources/shorthands.csv") as f:
+        lines = f.readlines()[1:]
+    return {l.split(",")[1].lower().strip(): l.split(",")[0].strip() for l in lines}
+
+
+def better_summary(old: str, shorthands: dict[str, str]) -> str:
     match = summary_regex.match(old)
     if match is None:
         return old
@@ -29,7 +37,9 @@ def better_summary(old: str) -> str:
     if " - " in name:
         [name, additional] = name.rsplit(" - ", 1)
 
-    # FIXME: add shorthands
+    if name.lower() in shorthands:
+        name = shorthands[name.lower()].upper()
+
     summary = f"{name} {lecture_type}"
     if additional != "":
         summary += " - " + additional
