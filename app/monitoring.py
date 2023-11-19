6779e2951508cfa1af1bc8b5f6900d4620c19c0b
flofriday <flohacksfriday@gmail.com>
2023-11-19 17:29:51 +0100

    Improve statistics query performance
    The improvement is only 2x which is still quite slow but better than
    nothing.

--- app/monitoring.py
+++ app/monitoring.py
@@ -61,15 +61,15 @@ def get_chart_data(db: Connection) -> list[Tuple[str, int]]:
                     COUNT(DISTINCT token_hash) AS 'daily',
                     (SELECT COUNT(DISTINCT token_hash)
                     FROM statistics s2
-                    WHERE Date(s2.date) <= Date(s.date) AND Date(s2.date) >= DATE( s.date, '-30 days')
+                    WHERE Date(s2.date) <= s.date AND s2.date >= DATE( s.date, '-30 days')
                 ) AS 'monbthly',
                 (SELECT COUNT(DISTINCT token_hash)
                     FROM statistics s3
-                    WHERE Date(s3.date) <=  Date(s.date)
+                    WHERE Date(s3.date) <=  s.date
                 ) AS 'total'
             FROM statistics s
             GROUP BY day
-            ORDER BY day
+            ORDER BY day 
         """
     )
     rows = cursor.fetchall()
